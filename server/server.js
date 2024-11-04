02b53afb59b802fdd6a06122c4aeeb59586f185e
HarshGuleri <harsh379.be22@chitkara.edu.in>
2024-11-04 13:45:56 +0530

    hanji ho gya

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDb();
 
 const app = express();
-const port = process.env.port || 5000 || 4000 || 4900 ||1000;
+const port = 5000 || 4000 || 4900 ||1000 || 2999 || 1024 || 8080;
 
 app.use(express.json());
 app.use(cors()); 
@@ -18,6 +18,21 @@ app.get('/',(req,res)=>{
     res.send("working");
 });
 
+app.get('/home',(req,res)=>{
+    res.render('home',{
+        username: "xyz",
+        posts: "flana dhimkana"
+    })
+})
+
+app.get('/allusers',(req,res)=>{
+    res.render('allusers',{
+        data:[{name:"abc", age:20},
+            {name:"def", age:19}]
+    })
+})
+app.use("/api/register" , require("./routes/userRoutes"));
+app.use("/api/doctorRegister" , require("./routes/doctorsDetails.js"));
 app.listen(port,()=>{
     console.log(`server running on port http://localhost:${port}`);
 });
